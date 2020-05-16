e0c2a3c9c9ddb3bb47646456c628654d09307011
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-05-16 06:09:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,6 +7,24 @@ def game_hash
       :players => [
         {
           :player_name => "Alan Anderson",
+          :number => "0",
+          :shoe => "16",
+          :points => "22",
+          :rebounds => "12",
+          :assists => "12",
+          :steals => "3",
+          :blocks => "1",
+          :slam_dunks => "1"
+        }, {
+          :player_name => "Reggie Evans",
+          :number => "0",
+          :shoe => "16",
+          :points => "22",
+          :rebounds => "12",
+          :assists => "12",
+          :steals => "3",
+          :blocks => "1",
+          :slam_dunks => "1"
         }],
     },
     :away => {

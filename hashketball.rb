e3c3c0ee5e2c84cde66861f25b1fa64c0f9966fc
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-05-16 06:03:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,18 @@
 # Write your code here!
+def game_hash
+  {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [
+        {
+          :player_name => 
+        }],
+    },
+    :away => {
+      :team_name => "",
+      :colors => [],
+      :players => [],
+    }
+  }
+end 
